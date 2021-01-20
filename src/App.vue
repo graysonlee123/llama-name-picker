434e4e1c63f39988ba66ec4f67ec927b734ffa8f
Grayson Gantek <graysonleegantek@gmail.com>
2021-01-19 21:32:25 -0500

    fixes css not loading

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
+    <navbar></navbar>
     {{ message }}
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
+
 export default {
+  components: {
+    Navbar,
+  },
   data() {
     return {
       message: 'Hello world',
@@ -19,5 +25,9 @@ export default {
   font-size: 1rem;
   font-family: 'Helvetica', 'Roboto', sans-serif;
   color: blue;
+}
+
+body {
+  background-color: tomato;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+      <li>
+        <a href="#">News</a>
+      </li>
+      <li>
+        <a href="#">Help</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+a {
+  color: red;
+}
+</style>
