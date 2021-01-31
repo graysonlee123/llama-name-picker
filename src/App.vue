407a330a8ef9c7395e55650d8b0ae1f7292aecec
Grayson Gantek <graysonleegantek@gmail.com>
2021-01-31 18:06:17 -0500

    adds components and initial working functionality

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,88 @@
 <template>
   <div id="app">
-    <navbar></navbar>
-    <hero></hero>
-    <featured></featured>
-    <div class="container mx-auto my-8">
-      <description-list></description-list>
+    <div class="container text-center mx-auto my-8">
+      <loader-button
+        @clicked="getData"
+        class="mb-8"
+        :text="chosenName ? 'New Name' : 'Get My Name'"
+        :loading="loading"
+      ></loader-button>
+      <p v-if="error" key="error">Error... Please try again later</p>
+      <p
+        v-else-if="loading"
+        key="loading"
+        class="text-sm font-bold text-indigo-500"
+      >
+        Loading...
+      </p>
+      <transition name="fade">
+        <p
+          v-if="chosenName"
+          key="name"
+          class="text-4xl font-extrabold text-indigo-500"
+        >
+          {{ chosenName }}
+        </p>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
-import Hero from './components/Hero.vue'
-import Featured from './components/Featured.vue'
-import Navbar from './components/Navbar.vue'
-import DescriptionList from './components/DescriptionList.vue'
+import LoaderButton from '@/components/LoaderButton.vue'
 
 export default {
-  components: {
-    Hero,
-    Featured,
-    Navbar,
-    DescriptionList,
+  components: { LoaderButton },
+  data() {
+    return {
+      options: {
+        count: 1,
+        type: 'male',
+        with_surname: true,
+      },
+      loading: false,
+      error: false,
+      chosenName: null,
+    }
+  },
+  methods: {
+    async getData() {
+      this.loading = true
+      this.error = false
+      this.chosenName = null
+
+      try {
+        setTimeout(async () => {
+          // const res = await fetch(
+          //   'https://namey.muffinlabs.com/name.json?' + this.query
+          // )
+          // const data = await res.json()
+
+          const data = ['John Johnson']
+
+          this.chosenName = data[0]
+          this.loading = false
+        }, 1000)
+      } catch (error) {
+        console.log(error)
+        this.loading = false
+        this.error = true
+      }
+    },
+  },
+  computed: {
+    query() {
+      return Object.keys(this.options)
+        .map(
+          key =>
+            encodeURIComponent(key) +
+            '=' +
+            encodeURIComponent(this.options[key])
+        )
+        .join('&')
+    },
   },
 }
 </script>
 
-<style>
-body {
-  background-color: lightblue;
-}
-</style>
+<style></style>
---
+++ src/components/LoaderButton.vue
@@ -0,0 +1,89 @@
+<template>
+  <button
+    @click="$emit('clicked')"
+    :data-text="text"
+    :class="{ loading }"
+  ></button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    loading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+button {
+  outline: none;
+  height: 40px;
+  text-align: center;
+  width: 160px;
+  border-radius: 40px;
+  background-color: transparent;
+  border-style: solid;
+  letter-spacing: 1px;
+  font-size: 12px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: all 0.25s ease;
+  text-transform: uppercase;
+
+  @apply text-indigo-500 border-indigo-500 border-2;
+}
+
+button:hover {
+  @apply text-white bg-indigo-500;
+}
+
+button:active {
+  letter-spacing: 2px;
+}
+
+button:focus {
+  outline: none;
+}
+
+button::after {
+  content: attr(data-text);
+}
+
+/* Loading */
+
+button.loading {
+  @apply border-gray-300 border-4 border-indigo-500;
+
+  width: 40px;
+  font-size: 0;
+  border-right-color: white;
+  border-bottom-color: white;
+  border-left-color: white;
+  animation: rotating 2s 0.25s linear infinite;
+  cursor: not-allowed;
+}
+
+button.loading:hover {
+  @apply bg-transparent text-indigo-500;
+}
+
+button.loading::after {
+  content: '';
+}
+
+@keyframes rotating {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
